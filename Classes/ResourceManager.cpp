e6a36f1429108f112edee36bb39d2d85e5f2ed89
Tuan <minhtuanapro@gmail.com>
2019-06-24 17:01:07 +0700

    + add ResourceManager GetInstance + init + Load

---
+++ Classes/ResourceManager.cpp
@@ -0,0 +1,99 @@
+#include "ResourceManager.h"
+#include <fstream>
+
+ResourceManager * ResourceManager::GetInstance()
+{
+	if (m_resource == nullptr)
+	{
+		m_resource = new ResourceManager();
+	}
+	return m_resource;
+}
+
+void ResourceManager::init(std::string path)
+{
+	this->m_dataFolderPath = path;	
+	this->Load("Data.bin");
+}
+
+void ResourceManager::Load(std::string fileName)
+{
+	std::ifstream infile;
+	
+	infile.open(fileName);
+
+	int id, lenght;
+	std::string ignore, path_normal, path_pressed;
+
+	if (infile.is_open())
+	{
+		infile >> ignore;
+		infile >> lenght;
+
+		for (int i = 0; i < lenght; i++)
+		{
+			infile >> ignore;
+			infile >> id;
+			infile >> ignore;
+			infile >> path_normal;
+
+			path_normal.replace(0, 2, this->m_dataFolderPath);
+
+			Sprite * sprite = Sprite::create(path_normal);
+
+			m_sprites.insert(std::pair<int, Sprite*>(id, sprite));
+		}
+	
+		infile >> ignore;
+		infile >> lenght;
+		
+		for (int i = 0; i < lenght; i++)
+		{
+			infile >> ignore;
+			infile >> id;
+			infile >> ignore;
+			infile >> path_normal;
+			infile >> ignore;
+			infile >> path_pressed;
+
+			path_normal.replace(0, 2, this->m_dataFolderPath);
+			path_pressed.replace(0, 2, this->m_dataFolderPath);
+
+			ui::Button *sprite = ui::Button::create(path_normal, path_pressed);
+
+			m_buttons.insert(std::pair<int, ui::Button*>(id, sprite));
+		}
+	
+		infile >> ignore;
+		infile >> lenght;
+	
+		for (int i = 0; i < lenght; i++)
+		{
+			infile >> ignore;
+			infile >> id;
+			infile >> ignore;
+			infile >> path_normal;
+
+			path_normal.replace(0, 2, this->m_dataFolderPath);
+
+			Label *sprite = Label::createWithTTF("", path_normal, 20);
+
+			m_labels.insert(std::pair<int, Label*>(id, sprite));
+		}
+	}
+}
+
+Sprite * ResourceManager::GetSpriteByID(int id)
+{
+	return nullptr;
+}
+
+ui::Button * ResourceManager::GetButtonByID(int id)
+{
+	return nullptr;
+}
+
+Label * ResourceManager::GetLabelByID(int id)
+{
+	return nullptr;
+}
---
+++ Classes/ResourceManager.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+#include <map>
+#include <string>
+
+USING_NS_CC;
+
+class ResourceManager
+{
+private:
+	static ResourceManager* m_resource;
+
+	ResourceManager();
+
+	std::string m_dataFolderPath;
+	
+	std::map <int, Sprite*> m_sprites;
+	std::map <int, ui::Button*> m_buttons;
+	std::map <int, Label*> m_labels;
+
+public:
+
+	static ResourceManager* GetInstance();
+	
+	void init(std::string path);
+	void Load(std::string fileName);
+	
+	Sprite* GetSpriteByID(int id);
+	ui::Button* GetButtonByID(int id);
+	Label* GetLabelByID(int id);
+};
