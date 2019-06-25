f365e1f1d6efdb4f6ee93c133152b500047f0f9e
Tuan <minhtuanapro@gmail.com>
2019-06-25 09:02:20 +0700

    + add Rock::Rock(Scene * scene)

--- Classes/Rock.cpp
+++ Classes/Rock.cpp
@@ -1,18 +1,23 @@
 #include "Rock.h"
+#include "ResourceManager.h"
 
 Rock::Rock(Scene * scene)
 {
-	
+	Sprite *rock = ResourceManager::GetInstance()->GetSpriteByID(3);
+	scene->addChild(rock);
 }
 
 Rock::~Rock()
 {
+
 }
 
 void Rock::Init()
 {
+
 }
 
 void Rock::Update(float deltaTime)
 {
+
 }
