cad09a878594767c81befb715d2201eeb17eedbb
Tuan <minhtuanapro@gmail.com>
2019-06-25 15:44:45 +0700

    + LoadingScene

--- Classes/LoadingScene.cpp
+++ Classes/LoadingScene.cpp
@@ -1,11 +1,19 @@
 #include "LoadingScene.h"
 #include "ResourceManager.h"
+#include "MenuScene.h"
 
+
+LoadingScene::LoadingScene()
+{
+}
+
+LoadingScene::~LoadingScene()
+{
+}
 
 Scene * LoadingScene::createScene()
 {
-	auto scene = LoadingScene::create();
-	return scene;
+	return LoadingScene::create();
 }
 
 bool LoadingScene::init()
@@ -16,21 +24,48 @@ bool LoadingScene::init()
 	}
 
 	auto visibleSize = Director::getInstance()->getVisibleSize();
-	Vec2 origin = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 	
-	auto background = ResourceManager::GetInstance()->GetSpriteByID(0);
+	// Add Background
+	background = ResourceManager::GetInstance()->GetSpriteByID(ID_BACKGROUND);
 	background->setPosition(visibleSize.width / 2, visibleSize.height / 2);
+	background->setScale(visibleSize.width / background->getContentSize().width,
+		visibleSize.height / background->getContentSize().height);
 	background->removeFromParent();
 	this->addChild(background,-10);
 
-	auto loadingBarBG = ResourceManager::GetInstance()->GetSpriteByID(5);
+	//Add Loading Bar BackGround
+	loadingBarBG = ResourceManager::GetInstance()->GetSpriteByID(ID_LOADINGBG);
 	loadingBarBG->setPosition(visibleSize.width / 2, origin.y + 64);
 	this->addChild(loadingBarBG, 1);
 
+	//Add Loading progess
+	loadingpress = ResourceManager::GetInstance()->GetSpriteByID(ID_LOADINGPRO);
+	loadingpress->setAnchorPoint(Vec2(0, 0.5));
+	loadingpress->setPosition(visibleSize.width  /2 - loadingpress->getContentSize().width / 2,
+		origin.y + 64);
+	loadingpress->setScale(0, 1);
+	this->addChild(loadingpress, 2);
+
+	//Add Label Loading
+	lb_loading = ResourceManager::GetInstance()->GetLabelByID(ID_LABEL);
+	lb_loading->setString("Loading...");
+	lb_loading->setPosition(visibleSize.width / 2, loadingpress->getPositionY());
+	this->addChild(lb_loading,3);
+
 	this->scheduleUpdate();
+
+	return true;
 }
 
 void LoadingScene::update(float deltaTime)
 {
+	scale += 0.01;
+	loadingpress->setScale(scale, 1);
+	if (scale >= 1)
+	{
+		this->unscheduleUpdate();
+		Director::getInstance()->replaceScene(MenuScene::createScene());
+	}
 
 }
--- Classes/LoadingScene.h
+++ Classes/LoadingScene.h
@@ -1,13 +1,26 @@
 #pragma once
 #include "cocos2d.h"
+#define ID_BACKGROUND 0
+#define ID_LOADINGBG 5
+#define ID_LOADINGPRO 6
+#define ID_LABEL 1
 
 USING_NS_CC;
 
 class LoadingScene :public Scene
 {
+private:
+	Sprite *background, *loadingBarBG, *loadingpress;
+	Label *lb_loading;
+	
+	float scale = 0;
 public:
+	LoadingScene();
+	virtual ~LoadingScene();
 	static Scene* createScene();
 	virtual bool init();
 
 	void update(float deltaTime) override;
+
+	CREATE_FUNC(LoadingScene);
 };
